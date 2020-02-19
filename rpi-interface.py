fc12d3486417dbaa1136cafef84dd29f4ed91d88
Kirsty Chau <kirstycha@gmail.com>
2020-02-18 19:04:48 -0700

    rpi GPIO interface using pigpio library

---
+++ rpi-interface.py
@@ -0,0 +1,156 @@
+import pigpio # library to contriol rpi pins
+from time import sleep
+
+class IO():
+    ''''
+    pin: 0 to 53 based off of pi broadcom number
+    KC: other funtionality to add? watchdog for pin
+    '''
+    def __init__(self, host = None, port = None):
+        self.pi = pigpio.pi(host, port)
+
+    def setMode(self, pin, output = 1):
+        if output:
+            self.pi.set_mode(pin, pigpio.OUTPUT)
+        else:
+            self.pi.set_mode(pin, pigpio.INPUT) 
+
+    def getMode(self, pin):
+        '''
+        0 = INPUT
+        1 = OUTPUT
+        Any other number is an ALT pin
+        '''
+        return self.pi.getMode(pin)
+        
+    def setInputMode(self, pin, pullup = 1):
+        '''
+        0 = pulldown
+        1 = pullup
+        else off
+        KC: check if pin set to be input?
+        '''
+        if pullup == 0:
+            self.pi.set_pull_up_down(pin, pigpio.PUD_DOWN)
+
+        elif pullup == 1:
+            self.pi.set_pull_up_down(pin, pigpio.PUD_UP)
+        else:
+            self.pi.set_pull_up_down(pin, pigpio.PUD_OFF)
+
+    def read(self, pin):
+        return self.pi.read(pin)
+
+    def write(self, pin, level):
+        self.pi.write(pin, level)
+
+    def toggle(self, pin):
+        '''
+        pin must be set as output for this to work
+        '''
+        state = self.read(pin)
+        state ^= 1 # performs xor operation with 1
+        self.write(pin, state)
+
+
+    def disconnect(self):
+        self.pi.stop()
+
+    def clkStart(self, pin, freq):
+        '''
+        sets pin to output low if not already set
+        uses the PWM functionality w 50% duty cycle
+        at the corresponding frequency
+        '''
+
+        # output zero 
+        if self.getMode(pin) != 1:
+            self.setMode(pin)
+        
+        self.write(pin, 0)
+
+        # set duty cycle to 50% out of 8 bit number
+        self.pi.set_PWM_dutycycle(pin, 128)
+
+        # the frequency is hardcoded so it pick the closest freq
+        self.pi.set_PWM_frequency(pin, freq)
+
+    def clkStop(self, pin):
+        self.pi.set_PWM_dutycycle(pin, 0)
+
+    def sendPulses(self, pin, numPulse, freq):
+
+        self.write(pin, 0)
+        delay = (2*freq)**-1
+        for i in range(numPulse):
+            sleep(delay)
+            self.write(pin, 1)
+            sleep(delay)
+            self.write(pin, 0)
+
+    def getFreq(self, pin):
+        return self.pi.get_PWM_frequency(pin)
+
+    
+    def generate_ramp(self, pin, ramp):
+        '''
+        Generate ramp wave forms.
+        ramp:  List of [Frequency, Steps], max length 20 
+        Steps: 0 to 65535
+        '''
+        self.pi.wave_clear()     # clear existing waves
+        length = len(ramp)  # number of ramp levels
+        wid = [-1] * length
+
+        # Generate a wave per ramp level
+        for i in range(length):
+            frequency = ramp[i][0]
+            micros = int(500000 / frequency)
+            wf = []
+            wf.append(pigpio.pulse(1 << pin, 0, micros))  # pulse on
+            wf.append(pigpio.pulse(0, 1 << pin, micros))  # pulse off
+            self.pi.wave_add_generic(wf)
+            wid[i] = self.pi.wave_create()
+
+        # Generate a chain of waves
+        chain = []
+        for i in range(length):
+            steps = ramp[i][1]
+            x = steps & 255
+            y = steps >> 8
+            chain += [255, 0, wid[i], 255, 1, x, y]
+
+        self.pi.wave_chain(chain)  # Transmit chain.
+
+if __name__ == "__main__":
+
+    # Set GPIO17/PIN11 for DIR control 
+    DIR = 17
+    
+    # Set GPIO27/PIN13 for PUL control 
+    PUL = 27
+
+    dut = IO()
+    #  Configure pins 
+    dut.setMode(DIR)
+    dut.setMode(PUL)
+
+    # set both low 
+    dut.write(DIR, 0)
+    dut.write(PUL, 0)
+
+    # test toggle
+    dut.toggle(DIR)
+    sleep(1)
+    dut.toggle(DIR)
+
+    # test clk function
+    dut.clkStart(PUL, 1)
+    sleep(10)
+    dut.clkStop(PUL)
+
+    # test ramp function
+    dut.generate_ramp(PUL, [
+        [320, 200],
+	    [500, 400],
+	    [800, 500]])

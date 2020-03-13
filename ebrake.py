74a81329c18492d7060fefd8be0bf5cec5fcea29
kirstyc <kirstycha@gmail.com>
2020-03-12 18:25:47 -0600

    fix for bouncy trigger

--- ebrake.py
+++ ebrake.py
@@ -1,45 +1,52 @@
+import time
 class Ebrake():
     def __init__(self, io, pinMBrake, pinEBrake):
         self.io = io
         self.pinMBrake = pinMBrake
-        self.pinEBrake = pinEBrake 
+        self.pinEBrake = pinEBrake
+        self.stateMBrake = 0 # open
+        self.stateEBrake = 0
+        self.flagBrake = 0  
 
     def setup(self, func = None):
-        self.setInputPin(self.pinMBrake, func)
         self.setOutputPin(self.pinEBrake)
+        self.setInputPin(self.pinMBrake, func)
 
     def setInputPin(self, pin, func = None):
         if func is None:
             func = self.mBrakeCallback
         self.pinMBrake = pin
+        # do not set pin to input, experimentally worse
         self.io.triggerCallback(pin, func, state = 2)
 
     def setOutputPin(self, pin):
         self.pinEbrake = pin
-        self.io.getMode(pin, output = 1)
-        self.io.write(pin, 0)
+        self.io.setMode(pin, output = 1)
+        self.io.write(pin, 1)
 
     def readMBrake(self):
         return self.io.read(self.pinMBrake)
 
     def setEbrake(self, on = 1):
         self.io.write(self.pinEBrake, on)
+        self.stateEBrake = on
+        self.flagBrake = on
 
     def mBrakeCallback(self, pin, level, tick):
         if pin != self.pinMBrake:
             return 
-        if level == 1:
-            # rising edge occured, notify pi 
-            # raise flag?
-            print('Manual brake: 1')
-        if level == 0:
-            # falling edge
-            # notify pi 
-            print('Manual brake: 0')
+        # trigger is bouncy read pin to see if state changed
+        state = self.readMBrake()
+        if state != self.stateMBrake:
+            self.flagBrake = state
+            self.stateMBrake = state
+            print(f'Level: {state}')
+            print(f'tick : {tick}')
+        # print('triggered callback, time: ',time.time())
         return level 
 
 if __name__ == "__main__":
-    import time
+    
     try:
         from .rpi_interface import IO
     except Exception:
@@ -53,9 +60,12 @@ if __name__ == "__main__":
 
     ebrake = Ebrake(gpio, SENSE, RELAY)
     ebrake.setup()
-    print("Ebrake: 1")
+    print("Ebrake: 1, OPEN")
     ebrake.setEbrake(1)
-    wait = ("Hit enter to set Ebrake: 0")
+    wait = input("Hit enter to set Ebrake: 0 CLOSE")
     ebrake.setEbrake(0)
     print("waiting for 5 seconds")
-    time.sleep()
+    time.sleep(5)
+    print("setting back to OPEN")
+    ebrake.setEbrake(1)
+    time.sleep(5)
